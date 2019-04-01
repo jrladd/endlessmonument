12392bcd869e312068a188ae4a5a5c21a10e2485
JR Ladd <johnrobertladd@gmail.com>
2019-04-01 16:01:39 -0500

    adjust for use without cron, test skyfield, better zipcode functions

--- zipcodes.py
+++ zipcodes.py
@@ -1,16 +1,23 @@
 import csv
-with open('/home/jrladd/twitterbots/epithalamibot/zips.csv', 'rb') as csvfile:
+with open('zips.csv', 'rb') as csvfile:
 	zipreader = csv.reader(csvfile)
 
 def findzipcode_withcitystate(city, state):
-	with open('/home/jrladd/twitterbots/epithalamibot/zips.csv', 'rb') as csvfile:
+	with open('zips.csv', 'rb') as csvfile:
 		zipreader = csv.reader(csvfile)
 		for row in zipreader:
 			if city in row[4] and state in row[1]:
 				return row[0]
 
+def findlatlong_withcitystate(city, state):
+	with open('zips.csv', 'r') as csvfile:
+		zipreader = csv.reader(csvfile)
+		for row in zipreader:
+			if city in row[4] and state in row[1]:
+				return row[2:4]
+
 def findzipcode_withlatlong(latitude, longitude):
-	with open('/home/jrladd/twitterbots/epithalamibot/zips.csv', 'rb') as csvfile:
+	with open('zips.csv', 'rb') as csvfile:
 		zipreader = csv.reader(csvfile)
 		for row in zipreader:
 			if latitude in row[2] and longitude in row[3]:
